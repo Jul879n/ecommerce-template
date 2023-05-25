4a25c27e2904ac980f03175f000533a6ec972fbe
Jul879n <l.araya@duocuc.cl>
2023-05-25 00:14:58 -0400

    actualizacion

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -13,22 +13,8 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-12 no-ver">
-				<?php include get_template_directory() . '/template-parts/registrarse.php'; ?>
-			</div>
-			<div class="col-12 col-sm-2 p-0 estatico no-ver">
-				<?php include get_template_directory() . '/template-parts/menu-lateral.php'; ?>
-			</div>
-			<div class="col-12 col-sm-2 p-0 no-ver">
-			</div>
-			<div class="col-12 col-sm-10 p-0 arriba mb-footer">
-				<?php include get_template_directory() . '/template-parts/nav-spotify.php'; ?>
-				<div class="playlists">
-					<?php include get_template_directory() . '/assets/modulos/modulo-playlists/loop-enfoque.php'; ?>
-					<?php include get_template_directory() . '/assets/modulos/modulo-playlists/loop-enfoque.php'; ?>
-				</div>
-				<?php include get_template_directory() . '/template-parts/footer.php'; ?>
-			</div>
+			<h1><?php echo get_the_title();?></h1>
+			<?php echo  get_the_content()?>
 		</div>
 	</div>
 </div><!-- #post-<?php the_ID(); ?> -->
