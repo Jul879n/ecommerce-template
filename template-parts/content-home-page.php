9133f7fd3ca0f1479ba5c7c2b7b55db8c3c912ef
Jul879n <l.araya@duocuc.cl>
2023-06-12 18:04:51 -0400

    Update content-home-page.php

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -157,7 +157,7 @@
 								<span><?php echo esc_html(get_field('beneficio_2_texto')); ?></span>
 							</div>
 							<div class="col-4">
-								<div class="d-flex">
+								<div class="d-flex border-start">
 								<?php
 								$image = get_field('beneficio_3_imagen');
 								if (!empty($image)): ?>
