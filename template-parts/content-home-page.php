2e7a487ff4af15a8fd65f0ce76c2d4f5fcded4c1
Jul879n <l.araya@duocuc.cl>
2023-06-12 16:27:46 -0400

    Update content-home-page.php

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -11,7 +11,35 @@
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<!--banner superior-->
 	
+	<div id="bannerSuperior" class="carousel slide">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<?php
+				$image = get_field('banner_1');
+				if (!empty($image)): ?>
+					<img class="banner" src="<?php echo esc_url($image['url']); ?>"
+						alt="<?php echo esc_attr($image['alt']); ?>" />
+				<?php endif; ?>
+			</div>
+			<div class="carousel-item">
+				<img src="..." class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+				<img src="..." class="d-block w-100" alt="...">
+			</div>
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#bannerSuperior" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#bannerSuperior" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+	<!--banner superior-->
 	<div class="container">
 		<div class="row">
 			<?php include get_template_directory() . '/assets/modulos/modulo-productos/loop-productos.php'; ?>
