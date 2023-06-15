3130698e7a238fd1e8a24b6f40094a98748268a3
Jul879n <l.araya@duocuc.cl>
2023-06-14 20:49:09 -0400

    Update content-home-page.php

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -270,19 +270,21 @@
 			</div>
 			<!--beneficios-->
 			<!--oferta del dia-->
-			<div class="card mb-3">
-				<div class="row">
-					<div class="col-md-7 p-0">
-						<?php
-						$image = get_field('imagen_del_dia');
-						if (!empty($image)) : ?>
-							<img class="img-fluid rounded-start" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-						<?php endif; ?>
-					</div>
-					<div class="col-md-5 bg-black text-light rounded-end d-flex align-items-center">
-						<div class="card-body ">
-							<span class="card-text text-uppercase"><?php echo esc_html(get_field('subtitulo_del_dia')); ?></span>
-							<h5 class="card-title text-uppercase"><?php echo esc_html(get_field('titulo_del_dia')); ?></h5>
+			<div class="col-12 col-sm-12">
+				<div class="card mb-3">
+					<div class="row">
+						<div class="col-md-7 p-0">
+							<?php
+							$image = get_field('imagen_del_dia');
+							if (!empty($image)) : ?>
+								<img class="img-fluid rounded-start" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+							<?php endif; ?>
+						</div>
+						<div class="col-md-5 bg-black text-light rounded-end d-flex align-items-center">
+							<div class="card-body ">
+								<span class="card-text text-uppercase"><?php echo esc_html(get_field('subtitulo_del_dia')); ?></span>
+								<h5 class="card-title text-uppercase"><?php echo esc_html(get_field('titulo_del_dia')); ?></h5>
+							</div>
 						</div>
 					</div>
 				</div>
