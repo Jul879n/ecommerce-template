3d4955eeb083118799deead6e0d0c63ff50ff9ec
Jul879n <l.araya@duocuc.cl>
2023-06-14 20:45:25 -0400

    Update content-home-page.php

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -15,66 +15,54 @@
 
 	<div id="bannerSuperior" class="carousel slide">
 		<div class="carousel-indicators">
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-				aria-current="true" aria-label="Slide 1"></button>
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-				aria-label="Slide 2"></button>
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-				aria-label="Slide 3"></button>
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
-				aria-label="Slide 4"></button>
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
-				aria-label="Slide 5"></button>
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"
-				aria-label="Slide 6"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
 		</div>
 		<div class="carousel-inner">
 			<div class="carousel-item active">
 				<?php
 				$image = get_field('banner_1');
-				if (!empty($image)): ?>
-					<img class="banner" src="<?php echo esc_url($image['url']); ?>"
-						alt="<?php echo esc_attr($image['alt']); ?>" />
+				if (!empty($image)) : ?>
+					<img class="banner" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 				<?php endif; ?>
 			</div>
 			<div class="carousel-item">
 				<?php
 				$image = get_field('banner_2');
-				if (!empty($image)): ?>
-					<img class="banner" src="<?php echo esc_url($image['url']); ?>"
-						alt="<?php echo esc_attr($image['alt']); ?>" />
+				if (!empty($image)) : ?>
+					<img class="banner" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 				<?php endif; ?>
 			</div>
 			<div class="carousel-item">
 				<?php
 				$image = get_field('banner_3');
-				if (!empty($image)): ?>
-					<img class="banner" src="<?php echo esc_url($image['url']); ?>"
-						alt="<?php echo esc_attr($image['alt']); ?>" />
+				if (!empty($image)) : ?>
+					<img class="banner" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 				<?php endif; ?>
 			</div>
 			<div class="carousel-item">
 				<?php
 				$image = get_field('banner_4');
-				if (!empty($image)): ?>
-					<img class="banner" src="<?php echo esc_url($image['url']); ?>"
-						alt="<?php echo esc_attr($image['alt']); ?>" />
+				if (!empty($image)) : ?>
+					<img class="banner" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 				<?php endif; ?>
 			</div>
 			<div class="carousel-item">
 				<?php
 				$image = get_field('banner_5');
-				if (!empty($image)): ?>
-					<img class="banner" src="<?php echo esc_url($image['url']); ?>"
-						alt="<?php echo esc_attr($image['alt']); ?>" />
+				if (!empty($image)) : ?>
+					<img class="banner" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 				<?php endif; ?>
 			</div>
 			<div class="carousel-item">
 				<?php
 				$image = get_field('banner_6');
-				if (!empty($image)): ?>
-					<img class="banner" src="<?php echo esc_url($image['url']); ?>"
-						alt="<?php echo esc_attr($image['alt']); ?>" />
+				if (!empty($image)) : ?>
+					<img class="banner" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 				<?php endif; ?>
 			</div>
 		</div>
@@ -155,9 +143,8 @@
 								<div class="col-12 col-sm-4">
 									<?php
 									$image = get_field('beneficio_1_imagen');
-									if (!empty($image)): ?>
-										<img class="banner" src="<?php echo esc_url($image['url']); ?>"
-											alt="<?php echo esc_attr($image['alt']); ?>" />
+									if (!empty($image)) : ?>
+										<img class="banner" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 									<?php endif; ?>
 									<span>
 										<?php echo esc_html(get_field('beneficio_1_texto')); ?>
@@ -166,9 +153,8 @@
 								<div class="col-12 col-sm-4">
 									<?php
 									$image = get_field('beneficio_2_imagen');
-									if (!empty($image)): ?>
-										<img class="banner" src="<?php echo esc_url($image['url']); ?>"
-											alt="<?php echo esc_attr($image['alt']); ?>" />
+									if (!empty($image)) : ?>
+										<img class="banner" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 									<?php endif; ?>
 									<span>
 										<?php echo esc_html(get_field('beneficio_2_texto')); ?>
@@ -178,9 +164,8 @@
 									<div class="d-flex align-items-center">
 										<?php
 										$image = get_field('beneficio_3_imagen');
-										if (!empty($image)): ?>
-											<img class="banner" src="<?php echo esc_url($image['url']); ?>"
-												alt="<?php echo esc_attr($image['alt']); ?>" />
+										if (!empty($image)) : ?>
+											<img class="banner" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 										<?php endif; ?>
 										<span>
 											<?php echo esc_html(get_field('beneficio_3_texto')); ?>
@@ -204,17 +189,13 @@
 				<div class="col-12 col-sm-4 mt-3">
 					<?php
 					$tarjeta_1 = get_field('tarjeta_1');
-					if ($tarjeta_1): ?>
+					if ($tarjeta_1) : ?>
 						<div class="card beneficio">
 							<a class="enlace" href="#">
-								<img class="fondo rounded-2" src="<?php echo esc_url($tarjeta_1['fondo']['url']); ?>"
-									alt="<?php echo esc_url($tarjeta_1['fondo']['url']); ?>" />
-								<div class="card-body rounded-bottom rounded-2"
-									style="background:linear-gradient(0deg, rgb(8,19,48) 0%, rgba(8,19,48, 0.0001) 100%)">
+								<img class="fondo rounded-2" src="<?php echo esc_url($tarjeta_1['fondo']['url']); ?>" alt="<?php echo esc_url($tarjeta_1['fondo']['url']); ?>" />
+								<div class="card-body rounded-bottom rounded-2" style="background:linear-gradient(0deg, rgb(8,19,48) 0%, rgba(8,19,48, 0.0001) 100%)">
 									<div class="caja d-flex align-items-end">
-										<img style="width: 80px; height: 80px;" class="rounded-3"
-											src="<?php echo esc_url($tarjeta_1['miniatura']['url']); ?>"
-											alt="<?php echo esc_attr($tarjeta_1['miniatura']['alt']); ?>" />
+										<img style="width: 80px; height: 80px;" class="rounded-3" src="<?php echo esc_url($tarjeta_1['miniatura']['url']); ?>" alt="<?php echo esc_attr($tarjeta_1['miniatura']['alt']); ?>" />
 										<div class="text-light ms-2">
 											<span class="ante-titulo text-uppercase">
 												<?php echo esc_html($tarjeta_1['antes_de_titulo']); ?>
@@ -235,17 +216,13 @@
 				<div class="col-12 col-sm-4 mt-3">
 					<?php
 					$tarjeta_2 = get_field('tarjeta_2');
-					if ($tarjeta_2): ?>
+					if ($tarjeta_2) : ?>
 						<div class="card beneficio">
 							<a class="enlace" href="#">
-								<img class="fondo rounded-2" src="<?php echo esc_url($tarjeta_2['fondo_2']['url']); ?>"
-									alt="<?php echo esc_url($tarjeta_2['fondo_2']['url']); ?>" />
-								<div class="card-body rounded-bottom rounded-2"
-									style="background:linear-gradient(0deg, rgb(60,3,78) 0%, rgba(60,3,78, 0.0001) 100%)">
+								<img class="fondo rounded-2" src="<?php echo esc_url($tarjeta_2['fondo_2']['url']); ?>" alt="<?php echo esc_url($tarjeta_2['fondo_2']['url']); ?>" />
+								<div class="card-body rounded-bottom rounded-2" style="background:linear-gradient(0deg, rgb(60,3,78) 0%, rgba(60,3,78, 0.0001) 100%)">
 									<div class="caja d-flex align-items-end">
-										<img style="width: 80px; height: 80px;" class="rounded-3"
-											src="<?php echo esc_url($tarjeta_2['miniatura_2']['url']); ?>"
-											alt="<?php echo esc_attr($tarjeta_2['miniatura_2']['alt']); ?>" />
+										<img style="width: 80px; height: 80px;" class="rounded-3" src="<?php echo esc_url($tarjeta_2['miniatura_2']['url']); ?>" alt="<?php echo esc_attr($tarjeta_2['miniatura_2']['alt']); ?>" />
 										<div class="text-light ms-2">
 											<span class="ante-titulo text-uppercase">
 												<?php echo esc_html($tarjeta_2['antes_de_titulo_2']); ?>
@@ -266,17 +243,13 @@
 				<div class="col-12 col-sm-4 mt-3">
 					<?php
 					$tarjeta_3 = get_field('tarjeta_3');
-					if ($tarjeta_3): ?>
+					if ($tarjeta_3) : ?>
 						<div class="card beneficio">
 							<a class="enlace" href="#">
-								<img class="fondo rounded-2" src="<?php echo esc_url($tarjeta_3['fondo_3']['url']); ?>"
-									alt="<?php echo esc_url($tarjeta_3['fondo_3']['url']); ?>" />
-								<div class="card-body rounded-bottom rounded-2"
-									style="background:linear-gradient(0deg, rgb(0,100,255) 0%, rgba(0,100,255, 0.0001) 100%)">
+								<img class="fondo rounded-2" src="<?php echo esc_url($tarjeta_3['fondo_3']['url']); ?>" alt="<?php echo esc_url($tarjeta_3['fondo_3']['url']); ?>" />
+								<div class="card-body rounded-bottom rounded-2" style="background:linear-gradient(0deg, rgb(0,100,255) 0%, rgba(0,100,255, 0.0001) 100%)">
 									<div class="caja d-flex align-items-end">
-										<img style="width: 80px; height: 80px;" class="rounded-3"
-											src="<?php echo esc_url($tarjeta_3['miniatura_3']['url']); ?>"
-											alt="<?php echo esc_attr($tarjeta_3['miniatura_3']['alt']); ?>" />
+										<img style="width: 80px; height: 80px;" class="rounded-3" src="<?php echo esc_url($tarjeta_3['miniatura_3']['url']); ?>" alt="<?php echo esc_attr($tarjeta_3['miniatura_3']['alt']); ?>" />
 										<div class="text-light ms-2">
 											<span class="ante-titulo text-uppercase">
 												<?php echo esc_html($tarjeta_3['antes_de_titulo_3']); ?>
@@ -295,28 +268,28 @@
 					<?php endif; ?>
 				</div>
 			</div>
-<!--beneficios-->
-<!--oferta del dia-->
-<div class="card mb-3">
-  <div class="row">
-    <div class="col-md-7 p-0">
-	<?php
-				$image = get_field('imagen_del_dia');
-				if (!empty($image)): ?>
-					<img class="img-fluid rounded-start" src="<?php echo esc_url($image['url']); ?>"
-						alt="<?php echo esc_attr($image['alt']); ?>" />
-				<?php endif; ?>
-    </div>
-    <div class="col-md-5 bg-black text-light rounded-end">
-      <div class="card-body">
-	  <span class="card-text"><?php echo esc_html(get_field('subtitulo_del_dia')); ?></span>
-        <h5 class="card-title"><?php echo esc_html(get_field('titulo_del_dia')); ?></h5>
-        
-      </div>
-    </div>
-  </div>
-</div>
-<!--oferta del dia-->
+			<!--beneficios-->
+			<!--oferta del dia-->
+			<div class="card mb-3">
+				<div class="row">
+					<div class="col-md-7 p-0">
+						<?php
+						$image = get_field('imagen_del_dia');
+						if (!empty($image)) : ?>
+							<img class="img-fluid rounded-start" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+						<?php endif; ?>
+					</div>
+					<div class="col-md-5 bg-black text-light rounded-end">
+						<div class="card-body d-flex align-items-center">
+							<div>
+								<span class="card-text text-uppercase">d<?php echo esc_html(get_field('subtitulo_del_dia')); ?></span>
+								<h5 class="card-title text-uppercase">d<?php echo esc_html(get_field('titulo_del_dia')); ?></h5>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--oferta del dia-->
 		</div>
 	</div>
 </div><!-- #post-<?php the_ID(); ?> -->
